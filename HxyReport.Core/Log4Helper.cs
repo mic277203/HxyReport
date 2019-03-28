b0fb01bef13aa348766b20c1d807c7f8058a3524
linph <linph@LINPH>
2019-03-28 11:07:33 +0800

    add log

---
+++ HxyReport.Core/Log4Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"config\Log4Net.config", Watch = true)]
+namespace HxyReport.Core
+{
+    public class Log4Helper
+    {
+        private static readonly ILog LogError = LogManager.GetLogger("LogError");
+        private static readonly ILog LogInfo = LogManager.GetLogger("LogInfo");
+
+        /// <summary>
+        /// 创建Error log ，对应配置文件中的LogError节点
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void ErrorLog(string msg)
+        {
+            LogError.Error(msg);
+        }
+
+        /// <summary>
+        /// 创建Error log ，对应配置文件中的LogError节点
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="ex"></param>
+        public static void ErrorLog(string msg, Exception ex)
+        {
+            LogError.Error(msg, ex);
+        }
+
+        /// <summary>
+        /// 创建Debugger log ，对应配置文件中的LogDebug节点
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void InfoLog(string msg)
+        {
+            LogInfo.Info(msg);
+        }
+    }
+}
--- HxyReport.MVC/Controllers/HomeController.cs
+++ HxyReport.MVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using HxyReport_MVC.Models;
 using Microsoft.AspNet.Identity;
 using System.Threading.Tasks;
+using HxyReport.Core;
 
 namespace HxyReport_MVC.Controllers
 {
@@ -16,11 +17,18 @@ namespace HxyReport_MVC.Controllers
         private readonly HxyUserManager _UserManager;
         public HomeController(HxyUserManager userManager)
         {
+            
             _UserManager = userManager;
         }
 
         public async Task<ActionResult> Index()
         {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Log4Helper.InfoLog("hello");
+                Log4Helper.ErrorLog("hello");
+            }
+           
             var user =await _UserManager.FindByIdAsync(User.Identity.GetUserId());
 
            return View();
